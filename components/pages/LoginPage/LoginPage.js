5a33c5a9654ccee8065d6ecdc76e53904f504a4a
hanna-buhai <hannah97bugai@gmail.com>
2023-02-01 13:57:04 +0300

    add base of login page UI

--- components/pages/LoginPage/LoginPage.js
+++ components/pages/LoginPage/LoginPage.js
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { useState } from 'react'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 import { PAGES } from '../../../constants/navigation'
 
 const styles = StyleSheet.create({
@@ -8,17 +9,52 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  username: {
+    width: '80%',
+    textAlign: 'left',
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    marginTop: 8,
+    marginBottom: 24,
+    borderWidth: 1,
+    padding: 8,
+    borderRadius: 4,
+  },
+  loginButton: {
+    minWidth: 200,
+  },
 })
 
 const LoginPage = ({ navigation }) => {
+  const [username, onChangeUsername] = useState('')
+
   const openCryptoListPage = () => {
     navigation.navigate(PAGES.CRYPTO_LIST_PAGE)
   }
 
+  const login = () => {
+    if (!!username) {
+      openCryptoListPage()
+      // TODO save user data
+      onChangeUsername('')
+    } else {
+      // TODO display an error
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>This is a Login Page</Text>
-      <Button title="Go to Crypto List" onPress={openCryptoListPage} />
+      <Text style={styles.username}>Username:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeUsername}
+        value={username}
+        autoFocus
+        textContentType={'username'}
+      />
+      <Button style={styles.loginButton} title={'Login'} onPress={login} />
     </View>
   )
 }
